d34ef0eaf8eb5c03980ac18090e2e09fbe9749cf
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2025-04-06 12:01:07 +0200

    some attributes

--- src/valhalla_export_tiles.cc
+++ src/valhalla_export_tiles.cc
@@ -1,5 +1,6 @@
 
 #include <cxxopts.hpp>
+#include <valhalla/baldr/attributes_controller.h>
 #include <valhalla/baldr/graphid.h>
 #include <valhalla/baldr/graphreader.h>
 #include <valhalla/mjolnir/graphtilebuilder.h>
@@ -14,6 +15,90 @@
 #include <ogrsf_frmts.h>
 
 namespace {
+
+struct AttributeFilter {
+  AttributeFilter(const std::vector<std::string>& includes,
+                  const std::vector<std::string>& excludes) {
+    for (const auto& include : includes) {
+      if (include == valhalla::baldr::kEdgeId) {
+        localidx = true;
+        continue;
+      }
+      if (include == valhalla::baldr::kEdgeRoadClass) {
+        road_class = true;
+        continue;
+      }
+      if (include == valhalla::baldr::kEdgeUse) {
+        use = true;
+        continue;
+      }
+      if (include == valhalla::baldr::kEdgeSpeed) {
+        speed = true;
+        continue;
+      }
+      if (include == valhalla::baldr::kEdgeTunnel) {
+        tunnel = true;
+        continue;
+      }
+      if (include == valhalla::baldr::kEdgeBridge) {
+        bridge = true;
+        continue;
+      }
+      if (include == valhalla::baldr::kEdgeTraversability) {
+        traversability = true;
+        continue;
+      }
+      if (include == valhalla::baldr::kEdgeSurface) {
+        surface = true;
+        continue;
+      }
+    }
+
+    for (const auto& exclude : excludes) {
+      if (exclude == valhalla::baldr::kEdgeId) {
+        localidx = false;
+        continue;
+      }
+      if (exclude == valhalla::baldr::kEdgeRoadClass) {
+        road_class = false;
+        continue;
+      }
+      if (exclude == valhalla::baldr::kEdgeUse) {
+        use = false;
+        continue;
+      }
+      if (exclude == valhalla::baldr::kEdgeSpeed) {
+        speed = false;
+        continue;
+      }
+      if (exclude == valhalla::baldr::kEdgeTunnel) {
+        tunnel = false;
+        continue;
+      }
+      if (exclude == valhalla::baldr::kEdgeBridge) {
+        bridge = false;
+        continue;
+      }
+      if (exclude == valhalla::baldr::kEdgeTraversability) {
+        traversability = false;
+        continue;
+      }
+      if (exclude == valhalla::baldr::kEdgeSurface) {
+        surface = false;
+        continue;
+      }
+    }
+  }
+
+  bool localidx{true};
+  bool road_class{true};
+  bool use{true};
+  bool speed{false};
+  bool tunnel{false};
+  bool bridge{false};
+  bool traversability{false};
+  bool surface{false};
+};
 
 OGRLineString* ConvertToOGRLineString(const std::vector<PointLL>& points) {
   OGRLineString* line = new OGRLineString();
@@ -49,7 +134,8 @@ void export_tile(valhalla::baldr::GraphReader& reader,
                  valhalla::Api& request,
                  valhalla::sif::cost_ptr_t costing,
                  GDALDriver* gdal_driver,
-                 char** dataset_options) {
+                 char** dataset_options,
+                 const AttributeFilter& filter) {
   // get the file path
   auto suffix =
       valhalla::baldr::GraphTile::FileSuffix(tile_id.Tile_Base(), ".fgb");
@@ -81,6 +167,16 @@ void export_tile(valhalla::baldr::GraphReader& reader,
     nodes_layer = dataset->CreateLayer("nodes", &spatialRef, wkbPoint,
                                        dataset_options);
 
+  // create fields
+  if (filter.localidx) {
+    OGRFieldDefn field_name("edgeid", OFTInteger);
+    edges_layer->CreateField(&field_name);
+  }
+  if (filter.road_class) {
+    OGRFieldDefn field_name("road_class", OFTString);
+    edges_layer->CreateField(&field_name);
+  }
+
   if ((edges && !edges_layer) || (nodes && !nodes_layer)) {
     LOG_ERROR("Failed to create layer.");
     GDALClose(dataset);
@@ -111,6 +207,15 @@ void export_tile(valhalla::baldr::GraphReader& reader,
     OGRFeature* feature =
         OGRFeature::CreateFeature(edges_layer->GetLayerDefn());
     feature->SetGeometry(line);
+
+    if (filter.localidx) {
+      feature->SetField("edgeid", static_cast<int>(idx));
+    }
+    if (filter.road_class) {
+      feature->SetField("road_class",
+                        valhalla::baldr::to_string(de->classification())
+                            .c_str());
+    }
     if (edges_layer->CreateFeature(feature) != OGRERR_NONE) {
       LOG_ERROR("Failed to create feature");
     }
@@ -127,6 +232,7 @@ void work(boost::property_tree::ptree& config,
           bool nodes,
           valhalla::Api& request,
           valhalla::sif::cost_ptr_t costing,
+          const AttributeFilter& filter,
           std::queue<valhalla::baldr::GraphId>& tile_queue,
           std::mutex& lock) {
 
@@ -153,7 +259,7 @@ void work(boost::property_tree::ptree& config,
       tile_queue.pop();
     }
     export_tile(reader, tile_id, output_dir, edges, nodes, request,
-                costing, driver, dataset_options);
+                costing, driver, dataset_options, filter);
   }
 }
 
@@ -169,6 +275,7 @@ void work(boost::property_tree::ptree& config,
  * @param request some request options, right now only used for the costing
  * and attribute controlling
  * @param costing the costing to filter allowed/disallowed edges
+ * @param filter which attributes to include/exclude
  * @param tile_ids which tiles to export
  */
 int export_tiles(boost::property_tree::ptree& config,
@@ -177,6 +284,7 @@ int export_tiles(boost::property_tree::ptree& config,
                  const bool nodes,
                  valhalla::Api& request,
                  valhalla::sif::cost_ptr_t costing,
+                 const AttributeFilter& filter,
                  std::vector<std::string>& tile_ids) {
 
   std::queue<valhalla::baldr::GraphId> tile_queue;
@@ -199,6 +307,7 @@ int export_tiles(boost::property_tree::ptree& config,
         std::make_shared<std::thread>(work, std::ref(config),
                                       std::cref(output_dir), edges, nodes,
                                       std::ref(request), costing,
+                                      std::cref(filter),
                                       std::ref(tile_queue),
                                       std::ref(lock));
   }
@@ -218,6 +327,8 @@ int main(int argc, char** argv) {
   std::vector<std::string> ftypes = {"edges"};
   valhalla::Api request;
   std::string costing_str;
+  std::vector<std::string> includes;
+  std::vector<std::string> excludes;
   bool edges = false;
   bool nodes = false;
 
@@ -233,13 +344,13 @@ int main(int argc, char** argv) {
     ("c,config", "Path to the json configuration file.", cxxopts::value<std::string>())
     ("i,inline-config", "Inline json config.",cxxopts::value<std::string>())
     ("o,costing", "Costing to use", cxxopts::value<std::string>())
-    ("a,attributes", "Attributes to include/exclude", cxxopts::value<std::string>())
+    ("e,exclude-attributes", "Attributes to exclude", cxxopts::value<std::vector<std::string>>())
+    ("a,include-attributes", "Attributes to include", cxxopts::value<std::vector<std::string>>())
     ("f,feature-type", "Feature types to output (currently supports node and edges, defaults to edges only)", cxxopts::value<std::vector<std::string>>())
     ("d,output-directory", "Directory in which output files will be written", cxxopts::value<std::string>())
     ("TILEID", "If provided, only export features matching the passed tile IDs. Can alternatively be passed via stdin", cxxopts::value<std::vector<std::string>>());
     // clang-format on
 
-    // options.custom_help("TILEID");
     options.positional_help("TILEID");
     options.parse_positional({"TILEID"});
 
@@ -288,6 +399,16 @@ int main(int argc, char** argv) {
       }
     }
 
+    if (result["include-attributes"].count() > 0) {
+      includes =
+          result["include-attributes"].as<std::vector<std::string>>();
+    }
+
+    if (result["exclude-attributes"].count() > 0) {
+      excludes =
+          result["exclude-attributes"].as<std::vector<std::string>>();
+    }
+
     costing_str = result["costing"].as<std::string>();
 
   } catch (cxxopts::exceptions::exception& e) {
@@ -303,9 +424,10 @@ int main(int argc, char** argv) {
     // register gdal drivers
     GDALAllRegister();
 
+    AttributeFilter filter(includes, excludes);
     valhalla::sif::cost_ptr_t costing = create_costing(costing_str);
     return export_tiles(pt, output_dir, edges, nodes, request, costing,
-                        tile_ids);
+                        filter, tile_ids);
   } catch (std::exception& e) {
     std::cout << "Failed to export tiles: " << e.what() << "\n";
     return EXIT_FAILURE;
